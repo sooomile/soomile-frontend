d7e28f9552c74b40c5d07225c5dd5ef684a418fb
nyun-nye <2001_11_7@naver.com>
2025-06-21 19:39:14 +0900

    🐛: 문제 해결

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import "./App.css";
 import Main from "./pages/main";
 
 function App() {
-  const [loading, error] = useKakaoLoader({
-    appkey: process.env.REACT_APP_KAKAOMAP_KEY,
-    libraries: ["services"],
-  });
-
   return (
     <div className="App">
       <Main />
