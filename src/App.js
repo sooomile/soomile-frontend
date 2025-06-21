fb16856e4712afbed3cec4ae7f3f5a116445d3b2
nyun-nye <2001_11_7@naver.com>
2025-06-21 19:13:19 +0900

    ✨: App에서 main 페이지 분리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
-import Header from "./components/header";
-import Map from "./components/map";
-import { useKakaoLoader } from "react-kakao-maps-sdk";
+import Main from "./pages/main";
 
 function App() {
-  const [loading, error] = useKakaoLoader({
-    appkey: process.env.REACT_APP_KAKAOMAP_KEY,
-    libraries: ["services"],
-  });
-
   return (
     <div className="App">
-      <Header />
-      {loading && <div>지도 로딩 중...</div>}
-      {error && (
-        <div>
-          <h2>지도 로딩 중 에러가 발생했습니다.</h2>
-          <p>{error.message}</p>
-        </div>
-      )}
-      {!loading && !error && <Map />}
+      <Main />
     </div>
   );
 }
