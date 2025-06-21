f0ab9f78eed84fb389341263ceb1fb21d7fbc3fb
nyun-nye <2001_11_7@naver.com>
2025-06-21 23:14:35 +0900

    🐛 : style 파일 버그 수정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Main from "./pages/main";
 import axios from "axios";
 import { API } from "./hooks/config";
+import SearchDaycareCenter from "./components/searchDaycareCenter";
 
 function App() {
   // axios.get(`${API.BASE_URL}daycares?name=명화`).then((res) => {
@@ -10,7 +11,8 @@ function App() {
 
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
+      <SearchDaycareCenter />
     </div>
   );
 }
