47aa7eb4d709ebf9cd89582dbbc26c6c73947904
nyun-nye <2001_11_7@naver.com>
2025-06-21 17:28:00 +0900

    ✨: 지도 불러오기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 import Main from "./pages/main";
 
 function App() {
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.REACT_APP_KAKAOMAP_KEY,
+    libraries: ["services"],
+  });
+
   return (
     <div className="App">
       <Main />
