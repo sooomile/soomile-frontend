4111309e17592666c737fd732ffbb0d0ecfbe36a
nyun-nye <2001_11_7@naver.com>
2025-06-21 17:50:36 +0900

    ✨: 현위치 마커 표시

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Map as KakaoMap, MapMarker } from "react-kakao-maps-sdk";
+import marker from "../assets/marker.svg";
+import currentLocation from "../assets/currentLocation.svg";
 
 const Map = () => {
+  const [location, setLocation] = useState({
+    lat: 37.5665, // 서울 시청
+    lng: 126.978, // 서울 시청
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (err) => {
+        console.error(err);
+        alert("위치 정보를 가져오는 데 실패했습니다.");
+        setIsLoading(false);
+      }
+    );
+  }, []);
+
   return (
     <KakaoMap
-      center={{ lat: 37.5665, lng: 126.978 }} // 서울 시청
+      center={location}
       style={{ width: "100%", height: "100vh" }}
       level={3}
     >
-      <MapMarker position={{ lat: 37.5665, lng: 126.978 }}>
-        <div style={{ padding: "5px", color: "#000" }}>서울시청</div>
-      </MapMarker>
+      {!isLoading && (
+        <MapMarker
+          position={location}
+          image={{
+            src: currentLocation,
+            size: {
+              width: 64,
+              height: 64,
+            },
+            options: {
+              // 기준점 이미지의 중앙으로 이동
+              offset: {
+                x: 32,
+                y: 32,
+              },
+            },
+          }}
+        ></MapMarker>
+      )}
     </KakaoMap>
   );
 };
