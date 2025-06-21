3d19d6903cd77cf1254f6077f79001560807aec8
nyun-nye <2001_11_7@naver.com>
2025-06-21 23:17:28 +0900

    ✨: 페이지 수정

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Main /> */}
-      <SearchDaycareCenter />
+      <Main />
+      {/* <SearchDaycareCenter /> */}
     </div>
   );
 }
