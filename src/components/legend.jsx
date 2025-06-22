3f2dfb4b0eb7fd0ef545fb9a3142367f4897433f
Parksh <2171164@hansung.ac.kr>
2025-06-22 12:59:12 +0900

    ✨ 범례 구성

---
+++ src/components/legend.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from '../styles/legend.module.scss';
+
+const Legend = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h3 className={styles.title}>
+          <span className={styles.highlight}>초미세먼지</span> 범례
+        </h3>
+        <button className={styles.button}>
+          <span>각 측정소별 월 미세먼지 평균보기</span>
+          <span>→</span>
+        </button>
+      </div>
+      <div className={styles.legend}>
+        <div className={styles.labels}>
+          <span>점검중</span>
+          <span>좋음</span>
+          <span>보통</span>
+          <span>나쁨</span>
+          <span>매우나쁨</span>
+        </div>
+        <div className={styles.colors}>
+          <div className={styles.checking}></div>
+          <div className={styles.good}></div>
+          <div className={styles.moderate}></div>
+          <div className={styles.bad}></div>
+          <div className={styles.veryBad}></div>
+        </div>
+        <div className={styles.ranges}>
+          <span></span>
+          <span>0-30</span>
+          <span>31-80</span>
+          <span>81-150</span>
+          <span>150초과</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Legend;
