e5af01cfb12fd299fb176f45d5d0381390c0b57d
nyun-nye <2001_11_7@naver.com>
2025-06-23 13:08:31 +0900

    ✨: 버튼 외부 링크로 연결

--- src/components/pmLegend.jsx
+++ src/components/pmLegend.jsx
@@ -1,14 +1,19 @@
 import styles from "../styles/pmLegend.module.scss";
 
 const PmLegend = () => {
+  const goToCleanAir = () => {
+    window.open(
+      "https://cleanair.seoul.go.kr/information/info12#emergency-response"
+    );
+  };
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.title}>
           <span style={{ color: "#4A3AFF" }}>미세먼지</span> 범례
         </div>
-        <div className={styles.modalButton}>
-          각 측정소별 월 미세먼지 평균보기 -{`>`}
+        <div className={styles.modalButton} onClick={goToCleanAir}>
+          미세먼지 예보등급 기준 보러가기 -{`>`}
         </div>
       </div>
       <div className={styles.body}>
@@ -28,7 +33,7 @@ const PmLegend = () => {
           </div>
           <div
             className={styles.barItem}
-            style={{ backgroundColor: "#ECFF96" }}
+            style={{ backgroundColor: "#FFC196" }}
           >
             나쁨
           </div>
@@ -39,13 +44,13 @@ const PmLegend = () => {
             매우나쁨
           </div>
         </div>
-        <div className={styles.caption}>
+        {/* <div className={styles.caption}>
           <div className={styles.captionItem}></div>
           <div className={styles.captionItem}>0-30</div>
           <div className={styles.captionItem}>31-80</div>
           <div className={styles.captionItem}>81-150</div>
           <div className={styles.captionItem}>150 초과</div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
